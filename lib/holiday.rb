0c8ca8d668a606f29ff1e90ad727dc9055224ed2
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-15 22:11:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,6 @@ def all_winter_holiday_supplies(holiday_hash)
      supplies.flatten
   end
   # return an array of all of the supplies that are used in the winter season
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
